76a9eb02fb0f912107f889a3c793694e12539d58
ChickeenS <piotrek1999w@gmail.com>
2022-01-13 08:54:30 +0100

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,16 @@
+
+$(".burger").on("click", function()
+{
+    $(".nav-mobile").toggleClass("nav-mobile--active")
+    $(".bar1").toggleClass("bar1--active")
+    $(".burger").toggleClass("burger--active")
+}
+)
+
+$(".link").on("click", function()
+{
+    $(".nav-mobile").toggleClass("nav-mobile--active")
+    $(".bar1").toggleClass("bar1--active")
+    $(".burger").toggleClass("burger--active")
+}
+)
